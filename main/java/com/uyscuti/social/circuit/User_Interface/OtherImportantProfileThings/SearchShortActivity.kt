27167a0b5c096f06f8b18e1d10e12a61a531c33f
JUMAOPPETER736 <jumapeter736@gmail.com>
2025-11-05 15:27:50 +0200

    The second approach is recommended because you're already using ViewBinding, so you should leverage it fully

--- main/java/com/uyscuti/social/circuit/User_Interface/OtherImportantProfileThings/SearchShortActivity.kt
+++ main/java/com/uyscuti/social/circuit/User_Interface/OtherImportantProfileThings/SearchShortActivity.kt
@@ -13,15 +13,15 @@ class SearchShortActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        setContentView(R.layout.activity_search_short)
+
         binding = ActivitySearchShortBinding.inflate(layoutInflater)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+
+        // Use binding.root or binding.main instead of findViewById
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        setContentView(binding.root)
-
     }
 }
