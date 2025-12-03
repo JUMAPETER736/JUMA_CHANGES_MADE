5b38435cb390bd9c4556f74030a89a6a709a767f
JUMAOPPETER736 <jumapeter736@gmail.com>
2025-12-03 16:55:26 +0200

    The enhanced logging will help you identify exactly where the data is getting lost

--- app/src/main/java/com/uyscuti/social/circuit/User_Interface/MyUserProfile/MyUserFavoritesFragment.kt
+++ app/src/main/java/com/uyscuti/social/circuit/User_Interface/MyUserProfile/MyUserFavoritesFragment.kt
@@ -379,7 +379,7 @@ class MyUserFavoritesFragment : Fragment(), OnFeedClickListener {
     override fun feedRepostPostClicked(position: Int, data: Post) {}
 
     override fun feedClickedToOriginalPost(position: Int, originalPostId: String) {}
-    
+
     override fun onImageClick() {}
 
 }
