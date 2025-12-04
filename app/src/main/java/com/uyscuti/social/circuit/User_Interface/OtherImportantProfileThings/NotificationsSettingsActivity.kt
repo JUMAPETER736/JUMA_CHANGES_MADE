6c9b8f572e49239792b3b436f17d68a7dcfb197a
JUMAOPPETER736 <jumapeter736@gmail.com>
2025-12-04 13:34:32 +0200

    This is cleaner and avoids potential null issues. Just make sure your layout file has a view with android:id=@+id/main.

--- app/src/main/java/com/uyscuti/social/circuit/User_Interface/OtherImportantProfileThings/NotificationsSettingsActivity.kt
+++ app/src/main/java/com/uyscuti/social/circuit/User_Interface/OtherImportantProfileThings/NotificationsSettingsActivity.kt
@@ -13,15 +13,14 @@ class NotificationsSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        setContentView(R.layout.activity_notifications_settings)
+
         binding = ActivityNotificationsSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)  // Move this BEFORE findViewById
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        setContentView(binding.root)
-
     }
 }
