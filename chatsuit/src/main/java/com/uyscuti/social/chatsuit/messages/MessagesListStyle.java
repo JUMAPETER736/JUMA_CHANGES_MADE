abb1befaa8a15eca0d068136ae7fab93b1d7a3b5
JUMAOPPETER736 <jumapeter736@gmail.com>
2025-12-15 09:28:54 +0200

    Code cleaning in the Message Input Style

--- chatsuit/src/main/java/com/uyscuti/social/chatsuit/messages/MessagesListStyle.java
+++ chatsuit/src/main/java/com/uyscuti/social/chatsuit/messages/MessagesListStyle.java
@@ -277,10 +277,6 @@ class MessagesListStyle extends Style {
         return incomingTextSize;
     }
 
-    protected int getIncomingTextStyle() {
-        return incomingTextStyle;
-    }
-
     protected Drawable getOutcomingBubbleDrawable() {
         if (outcomingBubbleDrawable == -1) {
             return getMessageSelector(outcomingDefaultBubbleColor, outcomingDefaultBubbleSelectedColor,
@@ -323,20 +319,12 @@ class MessagesListStyle extends Style {
         return outcomingTextSize;
     }
 
-    protected int getOutcomingTextStyle() {
-        return outcomingTextStyle;
-    }
-
     protected int getOutcomingTimeTextColor() {
         return outcomingTimeTextColor;
     }
 
     protected int getOutcomingTimeTextSize() {
         return outcomingTimeTextSize;
-    }
-
-    protected int getOutcomingTimeTextStyle() {
-        return outcomingTimeTextStyle;
     }
 
     protected int getOutcomingImageTimeTextColor() {
@@ -347,20 +335,12 @@ class MessagesListStyle extends Style {
         return outcomingImageTimeTextSize;
     }
 
-    protected int getOutcomingImageTimeTextStyle() {
-        return outcomingImageTimeTextStyle;
-    }
-
     protected int getDateHeaderTextColor() {
         return dateHeaderTextColor;
     }
 
     protected int getDateHeaderTextSize() {
         return dateHeaderTextSize;
-    }
-
-    protected int getDateHeaderTextStyle() {
-        return dateHeaderTextStyle;
     }
 
     protected int getDateHeaderPadding() {
@@ -375,10 +355,6 @@ class MessagesListStyle extends Style {
         return incomingTimeTextSize;
     }
 
-    protected int getIncomingTimeTextStyle() {
-        return incomingTimeTextStyle;
-    }
-
     protected int getIncomingTimeTextColor() {
         return incomingTimeTextColor;
     }
@@ -389,10 +365,6 @@ class MessagesListStyle extends Style {
 
     protected int getIncomingImageTimeTextSize() {
         return incomingImageTimeTextSize;
-    }
-
-    protected int getIncomingImageTimeTextStyle() {
-        return incomingImageTimeTextStyle;
     }
 
     protected Drawable getIncomingBubbleDrawable() {
