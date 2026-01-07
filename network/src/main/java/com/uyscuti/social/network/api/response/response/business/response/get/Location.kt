9f29bcae04ccf9ad3dbf815335b37df812c6b32d
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 10:56:35 +0200

    Fixing Duplicates Found in the Code Classes

--- network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/get/Location.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/get/Location.kt
@@ -1,6 +1,6 @@
 package com.uyscuti.social.network.api.response.business.response.get
 
-data class Location(
-    val businessLocation: BusinessLocation,
-    val walkingBillboard: WalkingBillboard
-)
+//data class Location(
+//    val businessLocation: BusinessLocation,
+//    val walkingBillboard: WalkingBillboard
+//)
--- network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/livelocation/LiveLocationResponse.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/livelocation/LiveLocationResponse.kt
@@ -2,7 +2,7 @@ package com.uyscuti.social.network.api.response.business.response.livelocation
 
 import com.uyscuti.social.network.api.response.business.response.background.UpdatedBusinessProfile
 
-data class LiveLocationResponse(
-    val message: String,
-    val updatedBusinessProfile: UpdatedBusinessProfile
-)
+//data class LiveLocationResponse(
+//    val message: String,
+//    val updatedBusinessProfile: UpdatedBusinessProfile
+//)
