83661be1216926737f6ee655643b4b7950ed3dd4
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 09:42:01 +0200

    Fixing Duplicates Found in the colors

--- network/src/main/java/com/uyscuti/social/network/api/response/posts/OriginalPost.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/posts/OriginalPost.kt
@@ -4,17 +4,17 @@ import java.io.Serializable
 
 
 
-data class OriginalUser(
-    val _id: String,
-    val avatar: Avatar,
-    val username: String,
-    val email: String,
-    val createdAt: String,
-    val updatedAt: String,
-    val coverImage: CoverImage,
-    val firstName: String,
-    val lastName: String,
-    val bio: String,
-    val owner: String
-): Serializable
+//data class OriginalUser(
+//    val _id: String,
+//    val avatar: Avatar,
+//    val username: String,
+//    val email: String,
+//    val createdAt: String,
+//    val updatedAt: String,
+//    val coverImage: CoverImage,
+//    val firstName: String,
+//    val lastName: String,
+//    val bio: String,
+//    val owner: String
+//): Serializable
 
