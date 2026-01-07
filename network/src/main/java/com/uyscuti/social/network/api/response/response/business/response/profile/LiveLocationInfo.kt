5517f7a4e95b157d32bdb2ef7b0188f76870c2cd
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:04:57 +0200

    Fixing Duplicates Found in the Code Classes

--- network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/profile/LiveLocationInfo.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/profile/LiveLocationInfo.kt
@@ -1,8 +1,8 @@
 package com.uyscuti.social.network.api.response.business.response.profile
 
-data class LiveLocationInfo(
-    val accuracy: String,
-    val latitude: String,
-    val longitude: String,
-    val range: String
-)
+//data class LiveLocationInfo(
+//    val accuracy: String,
+//    val latitude: String,
+//    val longitude: String,
+//    val range: String
+//)
