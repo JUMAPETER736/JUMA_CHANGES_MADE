66fca736814d62d3589639a214661b1688cdd2a0
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:05:57 +0200

    Fixing Duplicates Found in the Code Classes

--- network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/profile/ProfileResponse.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/profile/ProfileResponse.kt
@@ -5,18 +5,18 @@ import com.uyscuti.social.network.api.response.business.response.profile.Backgro
 import com.uyscuti.social.network.api.response.business.response.profile.Contact
 import com.uyscuti.social.network.api.response.business.response.profile.Location
 
-data class ProfileResponse(
-    val __v: Int,
-    val _id: String,
-    val backgroundPhoto: BackgroundPhoto?,
-    val backgroundVideo: BackgroundVideo?,
-    val businessCatalogue: List<Any>,
-    val businessDescription: String,
-    val businessName: String,
-    val businessType: String,
-    val contact: Contact,
-    val createdAt: String,
-    val location: Location,
-    val owner: String,
-    val updatedAt: String
-)
+//data class ProfileResponse(
+//    val __v: Int,
+//    val _id: String,
+//    val backgroundPhoto: BackgroundPhoto?,
+//    val backgroundVideo: BackgroundVideo?,
+//    val businessCatalogue: List<Any>,
+//    val businessDescription: String,
+//    val businessName: String,
+//    val businessType: String,
+//    val contact: Contact,
+//    val createdAt: String,
+//    val location: Location,
+//    val owner: String,
+//    val updatedAt: String
+//)
