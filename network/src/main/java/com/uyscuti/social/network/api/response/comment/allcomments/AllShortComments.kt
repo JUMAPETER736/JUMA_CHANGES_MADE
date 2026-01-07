e47a9f99f587a6dcde465672b9a54a7f67e00951
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:28:04 +0200

    Deleting directory Comments Response

--- network/src/main/java/com/uyscuti/social/network/api/response/comment/allcomments/AllShortComments.kt
+++
@@ -1,8 +0,0 @@
-package com.uyscuti.social.network.api.response.comment.allcomments
-
-data class AllShortComments(
-    val `data`: Data,
-    val message: String,
-    val statusCode: Int,
-    val success: Boolean
-)
--- network/src/main/java/com/uyscuti/social/network/api/response/comment/allcomments/Replies.kt
+++
@@ -1,5 +0,0 @@
-package com.uyscuti.social.network.api.response.comment.allcomments
-
-data class Replies(
-    val content: String
-)
