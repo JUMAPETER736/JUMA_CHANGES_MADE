bdfc7e573573159636a9acc51b6812a373b34085
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:48:46 +0200

    Deleting directory Post Response

--- network/src/main/java/com/uyscuti/social/network/api/response/post/Avatar.kt
+++
@@ -1,7 +0,0 @@
-package com.uyscuti.social.network.api.response.post
-
-data class Avatar(
-    val _id: String,
-    val localPath: String,
-    val url: String
-)
--- network/src/main/java/com/uyscuti/social/network/api/response/post/Thumbnail.kt
+++
@@ -1,7 +0,0 @@
-package com.uyscuti.social.network.api.response.post
-
-data class Thumbnail(
-    val _id: String,
-    val thumbnailLocalPath: String,
-    val thumbnailUrl: String
-)
