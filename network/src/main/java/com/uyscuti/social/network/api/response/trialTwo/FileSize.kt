3fb85fcb70a717dcff14bcc7846a2f524373a0b6
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:52:14 +0200

    Deleting directory Trial Two Response

--- network/src/main/java/com/uyscuti/social/network/api/response/trialTwo/FileSize.kt
+++
@@ -1,3 +0,0 @@
-package com.uyscuti.social.network.api.response.trialTwo
-
-class FileSize
--- network/src/main/java/com/uyscuti/social/network/api/response/trialTwo/FileType.kt
+++
@@ -1,6 +0,0 @@
-package com.uyscuti.social.network.api.response.trialTwo
-
-data class FileType(
-    val fileId: String,
-    val fileType: String
-)
