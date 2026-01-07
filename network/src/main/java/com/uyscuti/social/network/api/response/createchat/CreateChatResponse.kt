6e37753a2266427784e36debed48afe3f1d5f26f
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:29:36 +0200

    Deleting directory Create Chats Response

--- network/src/main/java/com/uyscuti/social/network/api/response/createchat/CreateChatResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.uyscuti.social.network.api.response.createchat
-
-data class CreateChatResponse(
-    val `data`: Data,
-    val message: String,
-    val statusCode: Int,
-    val success: Boolean
-)
