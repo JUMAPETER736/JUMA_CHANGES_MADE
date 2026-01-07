06e4cc7a8edf2a10dedd7616a36c38c20dc81297
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:03:52 +0200

    Fixing Duplicates Found in the Code Classes

--- network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/product/GetProductsResponse.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/product/GetProductsResponse.kt
@@ -2,9 +2,9 @@ package com.uyscuti.social.network.api.response.business.response.product
 
 import com.uyscuti.social.network.api.response.business.response.product.DataX
 
-data class GetProductsResponse(
-    val `data`: List<DataX>,
-    val message: String,
-    val statusCode: Int,
-    val success: Boolean
-)
+//data class GetProductsResponse(
+//    val `data`: List<DataX>,
+//    val message: String,
+//    val statusCode: Int,
+//    val success: Boolean
+//)
--- network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/profile/BackgroundVideo.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/profile/BackgroundVideo.kt
@@ -1,6 +1,6 @@
 package com.uyscuti.social.network.api.response.business.response.profile
 
-data class BackgroundVideo(
-    val thumbnail: String,
-    val url: String
-)
+//data class BackgroundVideo(
+//    val thumbnail: String,
+//    val url: String
+//)
