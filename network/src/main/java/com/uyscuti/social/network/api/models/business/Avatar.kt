f7acffc3105de786eacb1691ce82ebac84fb0f7a
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:09:07 +0200

    Fixing Duplicates Found in the Code Classes

--- network/src/main/java/com/uyscuti/social/network/api/models/business/Avatar.kt
+++ network/src/main/java/com/uyscuti/social/network/api/models/business/Avatar.kt
@@ -2,8 +2,8 @@ package com.uyscuti.social.network.api.models.business
 
 import java.io.Serializable
 
-data class Avatar(
-    val _id: String,
-    val localPath: String,
-    val url: String
-): Serializable
+//data class Avatar(
+//    val _id: String,
+//    val localPath: String,
+//    val url: String
+//): Serializable
