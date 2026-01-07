b3f31eb0f7bef3444f9719c0a6221b2d91d30021
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:45:48 +0200

    Deleting directory Last Seen Feed Example Response

--- network/src/main/java/com/uyscuti/social/network/api/response/latestFeedPostsExample/CoverImage.kt
+++
@@ -1,7 +0,0 @@
-package com.uyscuti.social.network.api.response.latestFeedPostsExample
-
-data class CoverImage(
-    val _id: String,
-    val localPath: String,
-    val url: String
-)
--- network/src/main/java/com/uyscuti/social/network/api/response/latestFeedPostsExample/FileType.kt
+++
@@ -1,6 +0,0 @@
-package com.uyscuti.social.network.api.response.latestFeedPostsExample
-
-data class FileType(
-    val fileId: String,
-    val fileType: String
-)
