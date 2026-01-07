221bc280ecc0f1df9f8807be7a6f74449be827d1
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:32:49 +0200

    Deleting directory Follow Un Follow Response

--- network/src/main/java/com/uyscuti/social/network/api/response/follow_unfollow/Data.kt
+++
@@ -1,5 +0,0 @@
-package com.uyscuti.social.network.api.response.follow_unfollow
-
-data class Data(
-    val following: Boolean
-)
--- network/src/main/java/com/uyscuti/social/network/api/response/follow_unfollow/OtherUsersFollowersAndFollowingResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.uyscuti.social.network.api.response.follow_unfollow
-
-data class OtherUsersFollowersAndFollowingResponse(
-    val success: Boolean,
-    val message: String,
-    val data: FollowStatusData
-)
-
-
