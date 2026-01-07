0b583c1e918df3431aa55ca08717a876e2c3592d
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:42:05 +0200

    Deleting directory Get Favorites Shorts Response

--- network/src/main/java/com/uyscuti/social/network/api/response/getfeedandresposts/Avatar.kt
+++
@@ -1,9 +0,0 @@
-package com.uyscuti.social.network.api.response.getfeedandresposts
-
-import java.io.Serializable
-
-data class Avatar(
-    val _id: String,
-    val localPath: String,
-    val url: String
-):Serializable
--- network/src/main/java/com/uyscuti/social/network/api/response/getfeedandresposts/NumberOfPage.kt
+++
@@ -1,3 +0,0 @@
-package com.uyscuti.social.network.api.response.getfeedandresposts
-
-class NumberOfPage
