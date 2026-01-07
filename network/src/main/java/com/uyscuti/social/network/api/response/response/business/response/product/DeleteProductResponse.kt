e2e939adda65311ee5c91eb520b346c906362244
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-07 11:02:31 +0200

    Fixing Duplicates Found in the Code Classes

--- network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/product/DeleteProductResponse.kt
+++ network/src/main/java/com/uyscuti/social/network/api/response/response/business/response/product/DeleteProductResponse.kt
@@ -1,8 +1,8 @@
 package com.uyscuti.social.network.api.response.business.response.product
 
-data class DeleteProductResponse(
-    val `data`: Any,
-    val message: String,
-    val statusCode: Int,
-    val success: Boolean
-)
+//data class DeleteProductResponse(
+//    val `data`: Any,
+//    val message: String,
+//    val statusCode: Int,
+//    val success: Boolean
+//)
