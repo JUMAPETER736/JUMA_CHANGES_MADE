bd0a984dac0e80a84755cba8512d29cb369779c1
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-08 09:46:10 +0200

    Resolving the Gradle KTS for the Main App and the Business

--- business/build.gradle.kts
+++ business/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-
-    id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -12,7 +11,6 @@ android {
 
     defaultConfig {
         minSdk = 26
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -26,10 +24,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -40,17 +40,17 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.swiperefreshlayout)
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    // Room
     val room_version = "2.6.1"
-
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     implementation("androidx.room:room-rxjava2:$room_version")
@@ -59,35 +59,44 @@ dependencies {
     implementation("androidx.room:room-paging:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
 
+    // Networking
     implementation("org.greenrobot:eventbus:3.3.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
 
+    // Image loading
     implementation("com.github.bumptech.glide:glide:4.15.1")
     kapt("com.github.bumptech.glide:compiler:4.15.1")
 
+    // UI components
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("com.tbuonomo:dotsindicator:5.0")
-    implementation ("io.getstream:photoview:1.0.2")
+    implementation("io.getstream:photoview:1.0.2")
 
+    // Media
     implementation("androidx.media3:media3-exoplayer:1.3.1")
     implementation("androidx.media3:media3-ui:1.3.1")
     implementation("androidx.media3:media3-extractor:1.3.1")
     implementation("androidx.media3:media3-exoplayer-dash:1.3.1")
 
+    // Google services
     implementation("com.google.android.gms:play-services-location:21.2.0")
+
+    // Lifecycle
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
+    // Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
 
+    // Animations
     implementation("com.daimajia.easing:library:2.4@aar")
     implementation("com.daimajia.androidanimations:library:2.4@aar")
-
     implementation("com.facebook.shimmer:shimmer:0.1.0@aar")
+
     // Emoji
     implementation("com.vanniktech:emoji-google-compat:0.21.0")
     implementation("com.vanniktech:emoji-twitter:0.21.0")
@@ -95,16 +104,17 @@ dependencies {
     implementation("com.vanniktech:emoji-google:0.21.0")
     implementation("com.vanniktech:emoji-ios:0.21.0")
 
+    // Document handling
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
     implementation("org.apache.poi:poi:5.2.4")
     implementation("org.apache.poi:poi-ooxml:5.2.4")
     implementation("org.apache.poi:poi-scratchpad:5.2.4")
 
+    // Image compression
     implementation("id.zelory:compressor:3.0.1")
 
-    implementation(project(":sharedModule"))
+    // Project modules
     implementation(project(":chatsuit"))
     implementation(project(":network"))
     implementation(project(":core"))
-
 }
