678027c70478c55e23ed0694c62898cbaa72328c
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-09 09:51:39 +0200

    In the CORE & Business Gradles issues

--- business/build.gradle.kts
+++ business/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     kotlin("kapt")
-    id("com.google.dagger.hilt.android")
+
 }
 
 android {
@@ -43,27 +43,17 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation(libs.androidx.swiperefreshlayout)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    // Room
-    val room_version = "2.6.1"
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    implementation("androidx.room:room-rxjava2:$room_version")
-    implementation("androidx.room:room-rxjava3:$room_version")
-    implementation("androidx.room:room-guava:$room_version")
-    implementation("androidx.room:room-paging:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
+
 
     // Networking
     implementation("org.greenrobot:eventbus:3.3.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
 
     // Image loading
     implementation("com.github.bumptech.glide:glide:4.15.1")
@@ -75,34 +65,28 @@ dependencies {
     implementation("io.getstream:photoview:1.0.2")
 
     // Media
-    implementation("androidx.media3:media3-exoplayer:1.3.1")
-    implementation("androidx.media3:media3-ui:1.3.1")
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
     implementation("androidx.media3:media3-extractor:1.3.1")
     implementation("androidx.media3:media3-exoplayer-dash:1.3.1")
 
     // Google services
-    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation(libs.play.services.location)
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-compiler:2.51.1")
+
 
     // Animations
     implementation("com.daimajia.easing:library:2.4@aar")
     implementation("com.daimajia.androidanimations:library:2.4@aar")
     implementation("com.facebook.shimmer:shimmer:0.1.0@aar")
 
-    // Emoji
+    // Emoji - Keep only one!
     implementation("com.vanniktech:emoji-google-compat:0.21.0")
-    implementation("com.vanniktech:emoji-twitter:0.21.0")
-    implementation("com.vanniktech:emoji-facebook:0.21.0")
-    implementation("com.vanniktech:emoji-google:0.21.0")
-    implementation("com.vanniktech:emoji-ios:0.21.0")
 
     // Document handling
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
@@ -114,7 +98,6 @@ dependencies {
     implementation("id.zelory:compressor:3.0.1")
 
     // Project modules
-
     implementation(project(":chatsuit"))
     implementation(project(":network"))
     implementation(project(":core"))
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 
-    id("com.google.dagger.hilt.android")
     id("kotlin-parcelize")
     kotlin("kapt")
 }
@@ -13,7 +12,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -41,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,20 +46,15 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-compiler:2.51.1")
 
-    implementation("androidx.room:room-runtime:2.6.0-alpha02")
-    implementation("androidx.room:room-ktx:2.6.0-alpha02")
-    kapt("androidx.room:room-compiler:2.6.0-alpha02")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    // Networking
     implementation("org.greenrobot:eventbus:3.3.1")
 
+    // Socket.IO
     implementation("io.socket:socket.io-client:2.0.0")
 
+    // Project modules
     implementation(project(":notifications"))
     implementation(project(":network"))
 }
