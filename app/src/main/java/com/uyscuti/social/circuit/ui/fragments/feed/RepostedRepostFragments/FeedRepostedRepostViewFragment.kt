73e2b89ab3861989da79c443adb1c0e8336401b6
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-30 11:03:50 +0200

    Making some Code changes in the Feed Reposted Repost View Fragment

--- app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/RepostedRepostFragments/FeedRepostedRepostViewFragment.kt
+++ app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/RepostedRepostFragments/FeedRepostedRepostViewFragment.kt
@@ -11,19 +11,30 @@ import com.uyscuti.sharedmodule.adapter.feed.feed.feedRepostedRepostViewAdapter.
 import com.uyscuti.social.circuit.databinding.FragmentFeedRepostedRepostViewBinding
 import com.uyscuti.sharedmodule.interfaces.feedinterfaces.FeedTextViewFragmentInterface
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FeedRepostedRepostViewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-private const val TAG = "FeedRepostedRepostViewFrag"
+
+private const val TAG = "FeedRepostedRepostViewFragment"
+
 class FeedRepostedRepostViewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String, originalPostId: String) =
+            FeedRepostedRepostViewFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+
+                    putString("originalPostId", originalPostId)
+                }
+            }
+    }
+
     private var param1: String? = null
     private var param2: String? = null
     private var adapter : FeedRostedViewAdapter?= null
@@ -64,27 +75,7 @@ class FeedRepostedRepostViewFragment : Fragment() {
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FeedRepostedRepostViewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String, originalPostId: String) =
-            FeedRepostedRepostViewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
 
-                    putString("originalPostId", originalPostId)
-                }
-            }
-    }
 
     fun setListener(listener: FeedTextViewFragmentInterface) {
         feedTextViewFragmentInterface = listener
