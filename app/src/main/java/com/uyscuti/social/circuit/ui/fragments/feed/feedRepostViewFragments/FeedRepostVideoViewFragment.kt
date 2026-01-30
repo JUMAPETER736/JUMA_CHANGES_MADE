dfcfdc218d4b545cce2ff0667a24850c673a6b41
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-30 11:02:51 +0200

    Making some Code changes in the Feed Repost Video View Fragment

--- app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostVideoViewFragment.kt
+++ app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostVideoViewFragment.kt
@@ -27,37 +27,37 @@ import com.uyscuti.social.circuit.databinding.FragmentFeedRepostVideoViewBinding
 import com.uyscuti.sharedmodule.feed_demo.VideoPagerAdapter
 import com.uyscuti.sharedmodule.interfaces.feedinterfaces.FeedTextViewFragmentInterface
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FeedRepostVideoViewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 private const val TAG = "FeedRepostVideoViewFragment"
 
 class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
-    // TODO: Rename and change types of parameters
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FeedRepostVideoViewFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var data: com.uyscuti.social.network.api.response.allFeedRepostsPost.OriginalPost
     private var position = 0
-    private var videoPlayingPosition = -1;
     private lateinit var backPressedCallback: OnBackPressedCallback
     private var feedTextViewFragmentInterface: FeedTextViewFragmentInterface? = null
 
     var adapter: FeedRepostMultipleAudioAdapter? = null
     private var adapter2: VideoPagerAdapter? = null
-
-    //    private var isPlaying = false
-    private var isUserSeeking = false
-    private var currentDuration: TextView? = null
-
-    //    private var reply: Boolean = false
-//
     var isPaused = false
 
     var videoUrl = ""
@@ -85,12 +85,7 @@ class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
             if (feedTextViewFragmentInterface != null) {
                 feedTextViewFragmentInterface?.backPressedFromFeedTextViewFragment()
                 adapter2?.backPressedFromFeedTextViewFragment()
-//                if (player != null) {
-//                    player?.removeListener(playbackStateListener)
-//                    player?.stop()
-//                    player?.release()
-//                    player?.clearVideoSurface()
-//                }
+
             }
         }
         Log.d(TAG, "onCreateView: data content ${data.content}")
@@ -118,24 +113,13 @@ class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
             Log.d(TAG, "render: data files is empty")
         }
         videoUrl = data.files[0].url
-        var previousPosition: Int = -1
-//        val videoUrls = listOf("url1", "url2", "url3")
-//        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
-//        viewPager.adapter = VideoPagerAdapter(this, videoUrls)
-        adapter2 = VideoPagerAdapter(requireActivity(), videoList)
-//        feedTextViewFragmentInterface?.let { adapter2!!.setFeedTextViewFragmentInterface(it) }
 
-//        adapter = MultipleFeedVideosAdapter(requireContext(), videoList, this)
+        adapter2 = VideoPagerAdapter(requireActivity(), videoList)
+
         binding.viewPager.adapter = adapter2
-        // Get the RecyclerView from ViewPager2
-//        val recyclerView: RecyclerView = binding.viewPager.getChildAt(0) as RecyclerView
-//
-//// Set the ItemAnimator to null to disable animations
-//        recyclerView.itemAnimator = null
+
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-//        binding.viewPager.registerOnPageChangeCallback(object :
-//            ViewPager2.OnPageChangeCallback() {
-//
+
         if (data.author.isEmpty()){
             Glide.with(this)
                 .load(data.author[0].account.avatar.url)
@@ -157,25 +141,7 @@ class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FeedRepostVideoViewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FeedRepostVideoViewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 
     override fun onResume() {
         Log.d(TAG, "onResume: ")
@@ -183,14 +149,10 @@ class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
         backPressedCallback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 // Handle back press
-//                navigateBack()
+
                 if (feedTextViewFragmentInterface != null) {
                     feedTextViewFragmentInterface?.backPressedFromFeedTextViewFragment()
-//                    if (player != null) {
-//                        player?.removeListener(playbackStateListener)
-//                        player?.stop()
-//                        player?.release()
-//                    }
+
                 }
             }
         }
@@ -199,8 +161,7 @@ class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
 
     override fun onDestroy() {
         super.onDestroy()
-//        player?.removeListener(playbackStateListener)
-//        player?.release()
+
     }
 
     fun setListener(listener: FeedTextViewFragmentInterface) {
