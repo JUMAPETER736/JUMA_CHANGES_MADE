09b9b72654f98bd17bbf9ebf8ef946f67a1b9061
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-30 10:47:11 +0200

    Making some Code changes in the Notification Host Fragment

--- app/src/main/java/com/uyscuti/social/circuit/ui/fragments/chat/NotificationHostFragment.kt
+++ app/src/main/java/com/uyscuti/social/circuit/ui/fragments/chat/NotificationHostFragment.kt
@@ -16,19 +16,27 @@ import com.uyscuti.social.circuit.databinding.FragmentNotificationHostBinding
 import com.uyscuti.social.circuit.ui.feed.NotificationsHostPagerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NotificationHostFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 @AndroidEntryPoint
 class NotificationHostFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            NotificationHostFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -56,7 +64,7 @@ class NotificationHostFragment : Fragment() {
         activity?.window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
         activity?.window?.navigationBarColor =
             ContextCompat.getColor(requireContext(), R.color.white)
-//        return inflater.inflate(R.layout.fragment_notification_host, container, false)
+
         return binding.root
     }
 
@@ -77,23 +85,5 @@ class NotificationHostFragment : Fragment() {
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotificationHostFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotificationHostFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
