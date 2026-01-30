25d313cd23fcfaaa58d2a6b12a84c155f06d06e3
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-01-30 10:58:34 +0200

    Making some Code changes in the Feed Image View Fragment

--- app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostMultipleImageFragment.kt
+++ app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostMultipleImageFragment.kt
@@ -19,19 +19,27 @@ import com.uyscuti.sharedmodule.adapter.feed.feed.MultipleImagesListener
 import com.uyscuti.social.circuit.R
 import com.uyscuti.sharedmodule.interfaces.feedinterfaces.FeedTextViewFragmentInterface
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FeedRepostMultipleImageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 private const val TAG = "FeedMultipleImageViewFragment"
+
 class FeedRepostMultipleImageFragment : Fragment() , MultipleImagesListener {
-    // TODO: Rename and change types of parameters
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FeedRepostMultipleImageFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var data: com.uyscuti.social.network.api.response.allFeedRepostsPost.OriginalPost
@@ -77,16 +85,13 @@ class FeedRepostMultipleImageFragment : Fragment() , MultipleImagesListener {
             .into(binding.toolbar.feedProfilePic)
 
         binding.toolbar.backIcon.setOnClickListener {
-//            feedTextViewFragmentInterface.onBackPressed()
-//            navigateBack()
+
             if (feedTextViewFragmentInterface != null) {
                 feedTextViewFragmentInterface?.backPressedFromFeedTextViewFragment()
             }
         }
         binding.commentButtonIcon.setOnClickListener {
-//            feedTextViewFragmentInterface?.onCommentClickFromFeedTextViewFragment(position, data)
-//            binding.feedCommentsCount.text = (data.comments + 1).toString()
-//            EventBus.getDefault().post(FeedCommentClicked(position, data))
+
         }
         val imageList:MutableList<String> = mutableListOf()
         if(data.files.isNotEmpty()) {
@@ -97,112 +102,21 @@ class FeedRepostMultipleImageFragment : Fragment() , MultipleImagesListener {
         }else {
             Log.d(TAG, "render: data files is empty")
         }
-// Setup ViewPager2 with the adapter
+
+        // Setup ViewPager2 with the adapter
         binding.viewPager.adapter = MultipleFeedImagesAdapter(requireContext(), imageList, this)
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         // Setup CircleIndicator for ViewPager2
-//        val indicator = findViewById<CircleIndicator3>(R.id.circleIndicator)
+
         binding.circleIndicator.setViewPager(binding.viewPager)
         binding.toolbar.username.text = data.author[0].account.username
-//        if (data.likes <= 0) {
-//            binding.likesCount.text = "0"
-//        } else {
-//            binding.likesCount.text = data.likes.toString()
-////        }
-//        binding.feedCommentsCount.text = data.comments.toString()
-//        if (data.isLiked) {
-//            binding.like.setImageResource(R.drawable.filled_favorite_like)
-//        } else {
-//            binding.like.setImageResource(R.drawable.like_svgrepo_com)
-//        }
-//        binding.like.setOnClickListener {
-//            data.isLiked = !data.isLiked
-//            if (data.isLiked) {
-//                Log.d(com.uyscut.flashdesign.ui.fragments.feed.feedviewfragments.TAG, "onCreateView: data likes ${data.likes}")
-//                binding.likesCount.text = data.likes.toString()
-//                if (data.likes < 0) {
-//                    binding.likesCount.text = "0"
-//                } else {
-//                    binding.likesCount.text = (data.likes + 1).toString()
-//                }
-//                binding.like.setImageResource(R.drawable.filled_favorite_like)
-//                YoYo.with(Techniques.Tada)
-//                    .duration(700)
-//                    .repeat(1)
-//                    .playOn(binding.like)
-//            } else {
-//                Log.d(com.uyscut.flashdesign.ui.fragments.feed.feedviewfragments.TAG, "onCreateView: data likes ${data.likes}")
-//                binding.likesCount.text = data.likes.toString()
-//                if (data.likes <= 0) {
-//                    binding.likesCount.text = "0"
-//                } else {
-//                    binding.likesCount.text = (data.likes - 1).toString()
-//                }
-//                binding.like.setImageResource(R.drawable.like_svgrepo_com)
-//                YoYo.with(Techniques.Tada)
-//                    .duration(700)
-//                    .repeat(1)
-//                    .playOn(binding.like)
-//            }
-//            feedTextViewFragmentInterface?.onLikeUnLikeFeedFromFeedTextViewFragment(position, data)
-//
-//        }
-//        if (data.isBookmarked) {
-//            binding.fav.setImageResource(R.drawable.filled_favorite)
-//        } else {
-//            binding.fav.setImageResource(R.drawable.favorite_svgrepo_com__1_)
-//        }
-//
-//        binding.moreOptions.setOnClickListener {
-//            feedTextViewFragmentInterface?.onMoreOptionsClickFromFeedTextViewFragment(
-//                position,
-//                data
-//            )
-//        }
-//        binding.fav.setOnClickListener {
-//            data.isBookmarked = !data.isBookmarked
-//            feedTextViewFragmentInterface?.onFeedFavoriteClickFromFeedTextViewFragment(
-//                position,
-//                data
-//            )
-//            if (data.isBookmarked) {
-//                binding.fav.setImageResource(R.drawable.filled_favorite)
-//                YoYo.with(Techniques.Tada)
-//                    .duration(700)
-//                    .repeat(1)
-//                    .playOn(binding.fav)
-//            } else {
-//                binding.fav.setImageResource(R.drawable.favorite_svgrepo_com__1_)
-//                YoYo.with(Techniques.Tada)
-//                    .duration(700)
-//                    .repeat(1)
-//                    .playOn(binding.fav)
-//            }
-//        }
+
 
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FeedRepostMultipleImageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FeedRepostMultipleImageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 
     override fun onResume() {
         Log.d(TAG, "onResume: ")
@@ -210,7 +124,7 @@ class FeedRepostMultipleImageFragment : Fragment() , MultipleImagesListener {
         backPressedCallback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 // Handle back press
-//                navigateBack()
+
                 if (feedTextViewFragmentInterface != null) {
                     feedTextViewFragmentInterface?.backPressedFromFeedTextViewFragment()
                 }
