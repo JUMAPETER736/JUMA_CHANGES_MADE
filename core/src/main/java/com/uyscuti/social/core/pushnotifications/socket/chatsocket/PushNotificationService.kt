3333f2572e72ffa4b4567282c0298ad8a6da64d8
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-02-11 11:58:22 +0200

    In the CORE PACKAGE deleting un used codes com.uyscuti.social.core.pushnotifications.socket.chatsocket, of the PushNotificationService

--- core/src/main/java/com/uyscuti/social/core/pushnotifications/socket/chatsocket/PushNotificationService.kt
+++ core/src/main/java/com/uyscuti/social/core/pushnotifications/socket/chatsocket/PushNotificationService.kt
@@ -3,23 +3,18 @@ package com.uyscuti.social.core.pushnotifications.socket.chatsocket
 
 import android.Manifest
 import android.app.IntentService
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresPermission
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.RemoteInput
 import com.uyscuti.social.core.local.utils.CoreStorage
 import com.uyscuti.social.core.local.utils.FileType
 import com.uyscuti.social.network.api.models.Message
-import com.uyscuti.social.notifications.R
 import com.uyscuti.social.notifications.di.RESULT_KEY
 import com.uyscuti.social.notifications.receiver.MyReceiver
 
@@ -27,7 +22,6 @@ import java.util.Locale
 
 
 class PushNotificationService : IntentService("NotificationService") {
-    private val CHANNEL_ID = "FlashChatNotifications"
     private val TAG = "PushNotificationService"
 
 
@@ -81,20 +75,7 @@ class PushNotificationService : IntentService("NotificationService") {
             }
             handleNotifications(chatId, notificationContent, sender)
 
-//            createNotificationChannel()
-//            showNotification(message)
-        }
-    }
 
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Chat Notifications"
-            val description = "Push Notifications for Chat"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(CHANNEL_ID, name, importance)
-            channel.description = description
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager.createNotificationChannel(channel)
         }
     }
 
@@ -132,7 +113,7 @@ class PushNotificationService : IntentService("NotificationService") {
             replyIntent,
             flag
         )
-        val replyAction = NotificationCompat.Action.Builder(
+        NotificationCompat.Action.Builder(
             0,
             "Reply",
             replyPendingIntent
@@ -143,26 +124,10 @@ class PushNotificationService : IntentService("NotificationService") {
                 Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return
         }
-//        notificationManage().notify(
-//            5858, notificationBuilder()
-//                .setContentTitle(user)
-//                .setContentText(message)
-//                .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                .setAutoCancel(true)
-//                .setWhen(currentTimeMillis)
-//                .addAction(replyAction)
-//                .build())
 
-        // Replace `5858` with a unique ID for your foreground service
         val foregroundNotificationId = 5858
 
 
@@ -173,7 +138,7 @@ class PushNotificationService : IntentService("NotificationService") {
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
             .setWhen(currentTimeMillis)
-//            .addAction(replyAction)
+
             .build()
 
 
@@ -208,48 +173,8 @@ class PushNotificationService : IntentService("NotificationService") {
         return NotificationCompat.Builder(this, "Chat_Notification")
             .setSmallIcon(com.uyscuti.social.notifications.R.drawable.ic_launcher_foreground)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
-//            .setStyle(notificationStyle)
+
             .addAction(replyAction)
     }
 
-    private fun notificationManage(): NotificationManagerCompat {
-        val notificationManager = NotificationManagerCompat.from(this)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                "Chat_Notification",
-                "Chat_Notification_Channel",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
-        return notificationManager
-    }
-
-    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
-    private fun showNotification(message: Message) {
-        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(message.sender.username)
-            .setContentText("${message.sender.username}: ${message.content}")
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-            .setAutoCancel(true) // Automatically remove the notification when clicked
-
-        val notificationManager = NotificationManagerCompat.from(this)
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        notificationManager.notify(message._id.hashCode(), notificationBuilder.build())
-    }
 }
