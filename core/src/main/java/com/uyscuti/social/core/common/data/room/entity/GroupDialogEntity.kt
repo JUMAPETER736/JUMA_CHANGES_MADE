a5aa073c5220637dec7460554bf69b5b535c79d5
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-01 12:00:41 +0200

    Modification of the Group Dialog

--- core/src/main/java/com/uyscuti/social/core/common/data/room/entity/GroupDialogEntity.kt
+++ core/src/main/java/com/uyscuti/social/core/common/data/room/entity/GroupDialogEntity.kt
@@ -1,5 +1,6 @@
 package com.uyscuti.social.core.common.data.room.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
@@ -9,7 +10,7 @@ import com.uyscuti.social.core.common.data.room.converters.UserConverter
 
 @Entity(tableName = "group_dialogs")
 @TypeConverters(UserConverter::class, MessageConverter::class)
-data class GroupDialogEntity (
+data class GroupDialogEntity(
     @PrimaryKey
     val id: String,
     val adminId: String,
@@ -20,5 +21,9 @@ data class GroupDialogEntity (
     var lastMessage: MessageEntity,
     var unreadCount: Int,
     val createdAt: Long,
-    val updatedAt: Long
+    val updatedAt: Long,
+    val description: String = "",
+
+    @ColumnInfo(name = "cachedMembersJson")
+    val cachedMembersJson: String? = null
 )
