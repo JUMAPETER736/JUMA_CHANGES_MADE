43c89c97b89dae719f87c1144a8ec5a9f3e1d00e
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-03 15:35:00 +0200

    feat: add GroupResult sealed class with Success, Error, and Loading states

--- sharedModule/src/main/java/com/uyscuti/sharedmodule/GroupProfileViewModel.kt
+++ sharedModule/src/main/java/com/uyscuti/sharedmodule/GroupProfileViewModel.kt
@@ -1,4 +1,32 @@
 package com.uyscuti.sharedmodule
 
-class GroupProfileViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.uyscuti.social.core.common.data.room.dao.GroupDialogDao
+import com.uyscuti.social.network.api.models.AddMembersRequest
+import com.uyscuti.social.network.api.models.AvatarData
+import com.uyscuti.social.network.api.models.ChangeRoleRequest
+import com.uyscuti.social.network.api.models.GroupChatDetail
+import com.uyscuti.social.network.api.models.GroupLinkData
+import com.uyscuti.social.network.api.models.GroupMember
+import com.uyscuti.social.network.api.models.GroupMemberUser
+import com.uyscuti.social.network.api.models.GroupRole
+import com.uyscuti.social.network.api.retrofit.instance.RetrofitInstance
+import com.uyscuti.social.network.utils.LocalStorage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+sealed class GroupResult<out T> {
+    data class Success<T>(val data: T) : GroupResult<T>()
+    data class Error(val message: String) : GroupResult<Nothing>()
+    object Loading : GroupResult<Nothing>()
 }
+
