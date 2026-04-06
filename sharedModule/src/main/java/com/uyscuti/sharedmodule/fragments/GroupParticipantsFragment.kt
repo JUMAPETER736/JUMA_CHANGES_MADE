9f643c796fd54151cb3223f025cd27fc38c9c066
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-06 09:00:00 +0200

    add GroupParticipantsFragment scaffold with companion object, ARG constants, and newInstance factory

--- sharedModule/src/main/java/com/uyscuti/sharedmodule/fragments/GroupParticipantsFragment.kt
+++ sharedModule/src/main/java/com/uyscuti/sharedmodule/fragments/GroupParticipantsFragment.kt
@@ -13,85 +13,28 @@ import com.uyscuti.sharedmodule.data.model.Dialog
 import com.uyscuti.sharedmodule.data.model.User
 import com.uyscuti.sharedmodule.R
 import com.uyscuti.sharedmodule.adapter.GroupParticipantAdapter
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GroupParticipantsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import dagger.hilt.android.AndroidEntryPoint
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
+@AndroidEntryPoint
 class GroupParticipantsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    private lateinit var groupParticipantAdapter: GroupParticipantAdapter
-    private lateinit var groupParticipantRecyclerView: RecyclerView
-    private lateinit var participantsNumber: TextView
-    private lateinit var dialog: Dialog
-    private lateinit var participantsList: List<User>
-    private lateinit var adminId: String
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            dialog = it.getParcelable("DIALOG")!!
-            participantsList = dialog.users
-            param1 = it.getString(ARG_PARAM1)
-            adminId = param1.toString()
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_group_participants, container, false)
-        groupParticipantRecyclerView = view.findViewById(R.id.participantsListRV)
-        groupParticipantRecyclerView.layoutManager = LinearLayoutManager(context)
-        participantsNumber = view.findViewById(R.id.participantsNumber)
-
-        val number = participantsList.size
-        val text = "$number participants"
-        participantsNumber.text = text
-
-//        Log.d("Participants", participantsList.toString())
-        for(i in participantsList) {
-
-        }
-
-        participantsList.map { user->
-            if (user.id == adminId) {
-                Log.d("Participants", "Admin is ${user.name}")
-            }
-        }
-
-        val sorted = participantsList.sortedWith(compareBy(
-            // First, sort by a custom order where "admin" comes first
-            { if (it.id == adminId) 0 else 1 },
-            // Then, sort alphabetically by name for other users
-            { it.name }
-        ))
-        groupParticipantAdapter = GroupParticipantAdapter(sorted)
-        groupParticipantAdapter.setAdminId(adminId)
-        groupParticipantRecyclerView.adapter = groupParticipantAdapter
-        return view
-    }
 
     companion object {
-        fun newInstance(dialog: Dialog, adminId: String, arg2: String): GroupParticipantsFragment {
-            val fragment = GroupParticipantsFragment()
-            val args = Bundle()
-            args.putParcelable("DIALOG", dialog)
-            args.putString(ARG_PARAM1, adminId)
-            args.putString(ARG_PARAM2, arg2)
-            fragment.arguments = args
-            return fragment
-        }
+        private const val ARG_ADMIN_ID = "adminId"
+        private const val ARG_MY_ROLE = "myRole"
+        private const val ARG_CHAT_ID = "chatId"
+
+        fun newInstance(chatId: String, adminId: String, myRole: String) =
+            GroupParticipantsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_CHAT_ID, chatId)
+                    putString(ARG_ADMIN_ID, adminId)
+                    putString(ARG_MY_ROLE, myRole)
+                }
+            }
     }
+
+    
+
+
 }
