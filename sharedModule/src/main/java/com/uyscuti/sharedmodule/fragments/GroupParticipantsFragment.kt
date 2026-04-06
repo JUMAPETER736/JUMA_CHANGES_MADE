1525fbd367d670c1d3536085e39a4b73c8ae85c1
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-06 10:10:00 +0200

    setup onViewCreated with SharedPrefs user info, MessageRepository init, and GroupMembersAdapter wiring

--- sharedModule/src/main/java/com/uyscuti/sharedmodule/fragments/GroupParticipantsFragment.kt
+++ sharedModule/src/main/java/com/uyscuti/sharedmodule/fragments/GroupParticipantsFragment.kt
@@ -74,6 +74,16 @@ class GroupParticipantsFragment : Fragment() {
         myRole  = arguments?.getString(ARG_MY_ROLE)  ?: "member"
     }
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_group_participants, container, false)
+        recyclerView       = view.findViewById(R.id.participantsListRV)
+        participantsNumber = view.findViewById(R.id.participantsNumber)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        return view
+    }
 
 }
