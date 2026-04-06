dff8861bd53c10decc144db116cc2c85db075c84
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-06 09:25:00 +0200

    declare RecyclerView, adapter, ViewModel, injected RetrofitInstance, LocalStorage, and user state fields

--- sharedModule/src/main/java/com/uyscuti/sharedmodule/fragments/GroupParticipantsFragment.kt
+++ sharedModule/src/main/java/com/uyscuti/sharedmodule/fragments/GroupParticipantsFragment.kt
@@ -7,13 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.uyscuti.sharedmodule.GroupProfileViewModel
 import com.uyscuti.sharedmodule.data.model.Dialog
 import com.uyscuti.sharedmodule.data.model.User
 import com.uyscuti.sharedmodule.R
 import com.uyscuti.sharedmodule.adapter.GroupParticipantAdapter
+import com.uyscuti.social.core.common.data.room.repository.MessageRepository
+import com.uyscuti.social.network.api.retrofit.instance.RetrofitInstance
+import com.uyscuti.social.network.utils.LocalStorage
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlin.getValue
 
 
 @AndroidEntryPoint
@@ -34,7 +41,31 @@ class GroupParticipantsFragment : Fragment() {
             }
     }
 
-    
+
+    private lateinit var recyclerView:       RecyclerView
+    private lateinit var participantsNumber: TextView
+    private lateinit var adapter:            GroupMembersAdapter
+    private lateinit var adminId:            String
+    private lateinit var myRole:             String
+
+    private val viewModel: GroupProfileViewModel by activityViewModels()
+
+    @Inject lateinit var retrofitInstance: RetrofitInstance
+    @Inject lateinit var localStorage: LocalStorage
+
+    private lateinit var messageRepository: MessageRepository
+
+    private var myUserId:   String = ""
+    private var myUsername: String = ""
+    private var myAvatar:   String = ""
+
+    // Cache the last known member list so we can still show it
+    // even after the current user has been removed from the group
+    private var cachedMembers: List<GroupMember> = emptyList()
+
+    // Track whether the current user has been removed
+    private var iWasRemoved: Boolean = false
+
 
 
 }
