f3a11f150dbaba8f7029afca781994a59385d62c
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-08 13:07:15 +0200

    In the Media Composition Activity creating the OnCreate & Set Up UI

--- sharedModule/src/main/java/com/uyscuti/sharedmodule/MediaCompositionActivity.kt
+++ sharedModule/src/main/java/com/uyscuti/sharedmodule/MediaCompositionActivity.kt
@@ -2,6 +2,8 @@ package com.uyscuti.sharedmodule
 
 import android.media.MediaPlayer
 import android.net.Uri
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.exoplayer.ExoPlayer
 import com.uyscuti.sharedmodule.databinding.ActivityMediaCompositionBinding
@@ -25,6 +27,30 @@ class MediaCompositionActivity : AppCompatActivity() {
         const val EXTRA_CAPTION = "CAPTION"
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMediaCompositionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get media URI from intent
+        mediaUri = intent.getParcelableExtra(EXTRA_MEDIA_URI)
+        val typeString = intent.getStringExtra(EXTRA_MEDIA_TYPE) ?: "UNKNOWN"
+        mediaType = MediaType.valueOf(typeString)
+
+        setupUI()
+        setupListeners()
+        loadMedia()
+    }
+
+    private fun setupUI() {
+        // Hide all views initially
+        binding.imagePreview.visibility = View.GONE
+        binding.videoPreview.visibility = View.GONE
+        binding.audioControls.visibility = View.GONE
+        binding.documentPreview.visibility = View.GONE
+
+
+    }
 
 
 }
