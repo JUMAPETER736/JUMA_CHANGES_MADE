47113e13c69f2fc0e2debd1cd666e73cfa6fbe13
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-12 17:32:11 +0200

    Making changes in the BusinessCatalogueAdapter

--- business/src/main/java/com/uyscuti/social/business/adapter/business/BusinessCatalogueAdapter.kt
+++ business/src/main/java/com/uyscuti/social/business/adapter/business/BusinessCatalogueAdapter.kt
@@ -3,33 +3,77 @@ package com.uyscuti.social.business.adapter.business
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.uyscuti.sharedmodule.data.model.User
+import com.uyscuti.social.core.models.data.User
 import com.uyscuti.social.business.R
 import com.uyscuti.social.business.adapter.business.postViewHolder.BusinessPostViewHolder
 import com.uyscuti.social.network.api.response.business.response.post.Post
 import com.uyscuti.social.network.api.retrofit.instance.RetrofitInstance
 import com.uyscuti.social.network.utils.LocalStorage
 
-
 class BusinessCatalogueAdapter(
     private val context: Activity,
     private val retrofitInterface: RetrofitInstance,
     private val localStorage: LocalStorage,
-    private var catalogue: MutableList<Post>,
     private val onItemClick: (Post) -> Unit = {},
     private var businessClickedListener: OnBusinessClickedListener,
     private val onBookmarkClick: (Post) -> Unit = { _ -> },
-//    private val onRepostClick: (BusinessCatalogue, Int) -> Unit = { _, _ -> },
-//    private val onShareClick: (BusinessCatalogue, Int) -> Unit = { _, _ -> },
     private val onFollowClick: (Post) -> Unit = { _ -> },
     private val onMessageClick: (User, Post) -> Unit = { _ , _-> },
-    private val fragmentManager: FragmentManager
+    private val onSendOfferClicked: (Double, String, Post) -> Unit = { _, _, _->},
+    private val fragmentManager: FragmentManager,
+    private val onLoadMore: (() -> Unit)? = null
+): ListAdapter<Post, RecyclerView.ViewHolder>(POST_COMPARATOR) {
 
-): RecyclerView.Adapter<BusinessPostViewHolder>() {
+    companion object {
+        private const val VIEW_TYPE_POST = 0
+        private const val VIEW_TYPE_LOADING = 1
+
+        private val POST_COMPARATOR = object : DiffUtil.ItemCallback<Post>() {
+            override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+                return oldItem._id == newItem._id
+            }
+
+            override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+                return oldItem.itemName == newItem.itemName &&
+                        oldItem.description == newItem.description &&
+                        oldItem.comments == newItem.comments &&
+                        oldItem.likes == newItem.likes &&
+                        oldItem.isLiked == newItem.isLiked &&
+                        oldItem.isBookmarked == newItem.isBookmarked
+            }
+
+            override fun getChangePayload(oldItem: Post, newItem: Post): Any? {
+                val changes = mutableMapOf<String, Any>()
+
+                if (oldItem.itemName != newItem.itemName) {
+                    changes["name"] = newItem.itemName
+                }
+                if (oldItem.description != newItem.description) {
+                    changes["description"] = newItem.description
+                }
+                if (oldItem.comments != newItem.comments) {
+                    changes["comments"] = newItem.comments
+                }
+                if (oldItem.likes != newItem.likes) {
+                    changes["likes"] = newItem.likes
+                }
+                if (oldItem.isLiked != newItem.isLiked) {
+                    changes["isLiked"] = newItem.isLiked
+                }
+                if (oldItem.isBookmarked != newItem.isBookmarked) {
+                    changes["isBookmarked"] = newItem.isBookmarked
+                }
+
+                return if (changes.isNotEmpty()) changes else null
+            }
+        }
+    }
 
     private var isLoadingMore = false
 
