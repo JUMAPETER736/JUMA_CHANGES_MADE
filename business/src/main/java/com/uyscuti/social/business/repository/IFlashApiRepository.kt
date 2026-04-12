f6e3dd7ae0b91598e90cd9c012cdbe47d2ed259d
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-12 18:35:22 +0200

    Making changes in the IFlashApiRepository & BusinessCatalogueViewModel

--- business/src/main/java/com/uyscuti/social/business/repository/IFlashApiRepository.kt
+++ business/src/main/java/com/uyscuti/social/business/repository/IFlashApiRepository.kt
@@ -1,12 +1,15 @@
 package com.uyscuti.social.business.repository
 
 import com.uyscuti.social.business.model.business.BusinessProfile
+import com.uyscuti.social.network.api.response.business.response.post.BusinessPost
 import com.uyscuti.social.network.api.response.business.response.post.Post
 
 interface IFlashApiRepository {
-    suspend fun getAllCatalogues(page: String): Result<List<Post>>
+    suspend fun getAllCatalogues(page: String): Result<BusinessPost>
     suspend fun getBusinessProfile(): Result<BusinessProfile>
-
-    suspend fun getUnreadNotifications(): Int
+    suspend fun searchCatalogues(
+        query: String,
+        page: String
+    ): Result<BusinessPost>
 
 }
