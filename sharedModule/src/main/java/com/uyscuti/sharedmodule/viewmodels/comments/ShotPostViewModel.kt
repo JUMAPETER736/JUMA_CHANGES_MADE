af1cf7f51cf8dda7c482d188e34fe21bbafc40dc
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-14 13:51:13 +0200

    Still Working with the ShotPostViewModel

--- sharedModule/src/main/java/com/uyscuti/sharedmodule/viewmodels/comments/ShotPostViewModel.kt
+++ sharedModule/src/main/java/com/uyscuti/sharedmodule/viewmodels/comments/ShotPostViewModel.kt
@@ -1,4 +1,76 @@
 package com.uyscuti.sharedmodule.viewmodels.comments
 
-class ShotPostViewModel {
-}
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.JsonSyntaxException
+import com.uyscuti.sharedmodule.utils.AudioDurationHelper.getFormattedDuration
+import com.uyscuti.sharedmodule.utils.generateMongoDBTimestamp
+import com.uyscuti.sharedmodule.utils.generateRandomId
+import com.uyscuti.sharedmodule.utils.getFileNameFromLocalPath
+import com.uyscuti.social.network.api.models.Comment
+import com.uyscuti.social.network.api.response.comment.CommentLocationData
+import com.uyscuti.social.network.api.response.comment.allcomments.Account
+import com.uyscuti.social.network.api.response.comment.allcomments.Author
+import com.uyscuti.social.network.api.response.comment.allcomments.Avatar
+import com.uyscuti.social.network.api.response.comment.allcomments.CommentFiles
+import com.uyscuti.social.network.api.retrofit.instance.RetrofitInstance
+import com.uyscuti.social.network.utils.LocalStorage
+import com.uyscuti.social.network.api.response.post.Data
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
+import javax.inject.Inject
+
+@HiltViewModel
+class ShotPostViewModel@Inject constructor(
+    private val retrofitInstance: RetrofitInstance,
+    private val localStorage: LocalStorage,
+    @ApplicationContext private val context: Context
+): ViewModel() {
+
+    val TAG = "ShotPostViewModel"
+
+    private val _commentsMutableLiveData: MutableLiveData<CommentState> = MutableLiveData()
+
+    private val settings =  context.getSharedPreferences("LocalSettings", MODE_PRIVATE)
+
+    val commentLiveData = _commentsMutableLiveData
+
+    fun likeUnlikeShotComment(commentId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            retrofitInstance.apiService.likeUnLikeComment(commentId)
+        }
+    }
+
+    fun likeUnlikeShotCommentReplies(commentReplyId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            retrofitInstance.apiService.likeUnLikeCommentReply(commentReplyId)
+        }
+    }
+
+
+    suspend fun getShotPost(postId: String): Data? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = retrofitInstance.apiService.getPostById(postId)
+                if (response.isSuccessful) {
+                    response.body()!!.data
+                } else {
+                    null
+                }
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
