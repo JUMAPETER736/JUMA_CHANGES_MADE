0acee7feb596487240a13459577aa7d20c5c79d3
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-18 12:55:17 +0200

    Creating the FeedRepostMultipleImageFragment

---
+++ app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostMultipleImageFragment.kt
@@ -0,0 +1,55 @@
+package com.uyscuti.social.circuit.ui.fragments.feed.feedRepostViewFragmentsimport
+import com.uyscuti.social.circuit.databinding.FragmentFeedRepostMultipleImageBinding
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
+import com.uyscuti.sharedmodule.adapter.feed.feed.MultipleFeedImagesAdapter
+import com.uyscuti.sharedmodule.adapter.feed.feed.MultipleImagesListener
+import com.uyscuti.social.circuit.R
+import com.uyscuti.sharedmodule.interfaces.feedinterfaces.FeedTextViewFragmentInterface
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FeedRepostMultipleImageFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+private const val TAG = "FeedMultipleImageViewFragment"
+class FeedRepostMultipleImageFragment : Fragment() , MultipleImagesListener {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var data: com.uyscuti.social.network.api.response.allFeedRepostsPost.OriginalPost
+    private var position = 0
+    private lateinit var backPressedCallback: OnBackPressedCallback
+    private var feedTextViewFragmentInterface: FeedTextViewFragmentInterface? = null
+
+    private lateinit var binding: FragmentFeedRepostMultipleImageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+            position = it.getInt("position")
+            data =
+                (it.getSerializable("data") as com.uyscuti.social.network.api.response.allFeedRepostsPost.OriginalPost?)!! // Adjust
+        }
+    }
+
+
+}
