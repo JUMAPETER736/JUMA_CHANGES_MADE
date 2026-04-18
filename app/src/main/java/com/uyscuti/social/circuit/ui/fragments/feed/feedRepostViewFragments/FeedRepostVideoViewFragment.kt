2d88d16cc6ecdc03fb39058e18cf80a58fdc57b8
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-18 15:04:02 +0200

    Adding new things FeedRepostVideoViewFragment

--- app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostVideoViewFragment.kt
+++ app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostVideoViewFragment.kt
@@ -42,6 +42,25 @@ private const val TAG = "FeedRepostVideoViewFragment"
 
 class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
 
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FeedRepostVideoViewFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FeedRepostVideoViewFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 
     private var param1: String? = null
     private var param2: String? = null
@@ -72,6 +91,24 @@ class FeedRepostVideoViewFragment : Fragment() , PlayFeedVideoInterface {
                 (it.getSerializable("data") as com.uyscuti.social.network.api.response.allFeedRepostsPost.OriginalPost?)!!
             position = it.getInt("position")
         }
+    }
+
+
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: ")
+        super.onResume()
+        backPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Handle back press
+
+                if (feedTextViewFragmentInterface != null) {
+                    feedTextViewFragmentInterface?.backPressedFromFeedTextViewFragment()
+
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, backPressedCallback)
     }
 
 }
