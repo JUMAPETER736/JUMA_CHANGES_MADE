dae4fe5e01d8426ba258d5a3aa8c8c3d2497ee26
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-04-18 12:35:50 +0200

    Finalizing the FeedRepostImageFragment

--- app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostImageFragment.kt
+++ app/src/main/java/com/uyscuti/social/circuit/ui/fragments/feed/feedRepostViewFragments/FeedRepostImageFragment.kt
@@ -35,17 +35,114 @@ class FeedRepostImageFragment : Fragment() {
     private lateinit var backPressedCallback: OnBackPressedCallback
     private var feedTextViewFragmentInterface: FeedTextViewFragmentInterface? = null
 
-    private lateinit var binding: FragmentFeedRepostImageBinding
+    private lateinit var binding : FragmentFeedRepostImageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
             position = it.getInt("position")
-            data =
-                (it.getSerializable("data") as com.uyscuti.social.network.api.response.allFeedRepostsPost.OriginalPost)// Adjust
+            data = (it.getSerializable("data") as com.uyscuti.social.network.api.response.allFeedRepostsPost. OriginalPost)// Adjust
         }
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding =  FragmentFeedRepostImageBinding.inflate(inflater, container, false)
+        if (data.content == "") {
+            binding.feedTextContent.text = ""
+        } else {
+            binding.feedTextContent.text = data.content
+        }
 
+
+        if (data.tags.isEmpty()) {
+            binding.tags.visibility = View.GONE
+        } else {
+            binding.tags.visibility = View.VISIBLE
+            val formattedTags = data.tags.joinToString(" ") { "#$it" }
+
+            binding.tags.text = formattedTags
+        }
+        Glide.with(this)
+            .load(data.files[0].url)
+            .placeholder(com.uyscuti.sharedmodule.R.drawable.profilepic2)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(binding.feedImage)
+
+        if (data.author.isEmpty()){
+            Glide.with(this)
+                .load(data.author[0].account.avatar.url)
+                .apply(RequestOptions.bitmapTransform(CircleCrop()))
+                .placeholder(com.uyscuti.sharedmodule.R.drawable.profilepic2)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(binding.toolbar.feedProfilePic)
+        }else{
+            Glide.with(this)
+                .load(com.uyscuti.sharedmodule.R.drawable.profilepic2)
+                .apply(RequestOptions.bitmapTransform(CircleCrop()))
+                .placeholder(com.uyscuti.sharedmodule.R.drawable.profilepic2)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(binding.toolbar.feedProfilePic)
+        }
+
+
+        binding.toolbar.backIcon.setOnClickListener {
+//            feedTextViewFragmentInterface.onBackPressed()
+//            navigateBack()
+            if (feedTextViewFragmentInterface != null) {
+                feedTextViewFragmentInterface?.backPressedFromFeedTextViewFragment()
+            }else{
+                Log.d(TAG, "onCreateView: feedTextViewFragmentInterface is null")
+            }
+        }
+        binding.commentButtonIcon.setOnClickListener {
+
+        }
+
+        return binding.root
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FeedRepostImageFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FeedRepostImageFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: ")
+        super.onResume()
+        backPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Handle back press
+//                navigateBack()
+                if (feedTextViewFragmentInterface != null) {
+                    feedTextViewFragmentInterface?.backPressedFromFeedTextViewFragment()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, backPressedCallback)
+    }
+
+    fun setListener(listener: FeedTextViewFragmentInterface) {
+        feedTextViewFragmentInterface = listener
+    }
 }
